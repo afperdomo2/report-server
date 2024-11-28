fddca04e9b981fc1eac7696a387f3d557267e241
alvarado0925 <alvarado0925@gmail.com>
2024-11-27 23:36:35 -0500

    Actualiza .gitignore, mejora README y elimina archivos no utilizados de la aplicación

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const logger = new Logger('Reports');
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  logger.log(`🚀 Running on PORT: 3000`);
 }
 bootstrap();
