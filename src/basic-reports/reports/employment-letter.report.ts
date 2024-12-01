c94b0c2ae911e28c7f6d86bfd2a23706c642df8a
alvarado0925 <alvarado0925@gmail.com>
2024-11-30 23:51:55 -0500

    Añade logo y pie de página al informe de carta de empleo, y ajusta estilos y márgenes

--- src/basic-reports/reports/employment-letter.report.ts
+++ src/basic-reports/reports/employment-letter.report.ts
@@ -1,11 +1,15 @@
-import { StyleDictionary, TDocumentDefinitions } from 'pdfmake/interfaces';
+import {
+  Content,
+  StyleDictionary,
+  TDocumentDefinitions,
+} from 'pdfmake/interfaces';
 
 const style: StyleDictionary = {
-  header: {
+  title: {
     fontSize: 18,
     bold: true,
     alignment: 'center',
-    margin: [0, 0, 0, 25],
+    margin: [0, 60, 0, 30],
   },
   body: {
     fontSize: 12,
@@ -18,11 +22,25 @@ const style: StyleDictionary = {
   },
 };
 
+const logo: Content = {
+  image: 'src/assets/tucan-code-logo.png',
+  width: 90,
+  height: 90,
+  alignment: 'right',
+};
+
 export const getEmploymentLetterReport = () => {
   const docDefinition: TDocumentDefinitions = {
     styles: style,
+    pageMargins: [40, 50, 40, 50],
+    header: {
+      columns: [
+        logo,
+        { text: `${new Date()}`, alignment: 'right', margin: [0, 20, 20, 0] },
+      ],
+    },
     content: [
-      { text: 'CONSTANCIA DE EMPLEO', style: 'header' },
+      { text: 'CONSTANCIA DE EMPLEO', style: 'title' },
       {
         text: `Yo, [Nombre del Empleador], en mi calidad de [Cargo del Empleador] de [Nombre de la Empresa], por medio de la presente certifico que [Nombre del Empleado] ha sido empleado en nuestra empresa desde el [Fecha de Inicio del Empleado].\n
         Durante su empleo, el Sr./Sra. [Nombre del Empleado] ha desempeñado el cargo de [Cargo del Empleado], demostrando responsabilidad, compromiso y habilidades profesionales en sus labores.\n
@@ -35,6 +53,12 @@ export const getEmploymentLetterReport = () => {
       { text: '[Cargo del Empleador]', style: 'signature' },
       { text: '[Fecha de Emisión]', style: 'signature' },
     ],
+    footer: {
+      text: 'Este documento es una constancia de empleo y no representa un compromiso laboral.',
+      fontSize: 10,
+      alignment: 'center',
+      margin: [0, 25, 0, 0],
+    },
   };
   return docDefinition;
 };
