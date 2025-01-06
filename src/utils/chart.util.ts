e092a02bac5defe5fc515ac8cee26c4f4b549771
alvarado0925 <alvarado0925@gmail.com>
2025-01-05 20:59:32 -0500

    Añade opciones de configuración para el tamaño de la imagen en chartJsToImage: permite especificar altura y anchura en la conversión de gráficos a imágenes PNG

--- src/utils/chart.util.ts
+++ src/utils/chart.util.ts
@@ -1,13 +1,28 @@
 import axios from 'axios';
 
+interface chartOptions {
+  height?: number;
+  width?: number;
+}
+
 /**
  * Convert chart.js configuration to image
  * @param chartConfig The chart.js configuration
  * @returns A promise that resolves to a base64 string
  */
-export const chartJsToImage = async (chartConfig: unknown) => {
+export const chartJsToImage = async (
+  chartConfig: unknown,
+  options: chartOptions = {},
+) => {
+  const params = new URLSearchParams();
+  if (options.height) {
+    params.append('height', options.height.toString());
+  }
+  if (options.width) {
+    params.append('width', options.width.toString());
+  }
   const encodedUri = encodeURIComponent(JSON.stringify(chartConfig));
-  const baseUrl = `https://quickchart.io/chart?c=${encodedUri}`;
+  const baseUrl = `https://quickchart.io/chart?c=${encodedUri}&${params.toString()}`;
   const response = await axios.get(baseUrl, { responseType: 'arraybuffer' });
   return `data:image/png;base64,${Buffer.from(response.data).toString('base64')}`;
 };
